76b8338ff0855e8de9e69700209e136537a216c4
Joseph Irving <joseph.irving500@gmail.com>
2017-01-16 21:11:53 +0000

    removed old flag

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-kd --insecure-skip-tls-verify --retries=20 \
+kd --insecure-skip-tls-verify \
    --file ${ENVIRONMENT}/pttg-fs-api-deployment.yaml \
    --file ${ENVIRONMENT}/pttg-fs-api-svc.yaml
