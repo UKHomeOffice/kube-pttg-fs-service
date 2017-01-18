407d06962d4ef0b5f9027bffdb27095b1bfd93f2
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2017-01-18 09:35:40 +0000

    Set timeout to 5 minutes and update kd version to 0.2.3

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-kd --insecure-skip-tls-verify  --timeout 300 \
+kd --insecure-skip-tls-verify  --timeout 5m0s \
    --file ${ENVIRONMENT}/pttg-fs-api-deployment.yaml \
    --file ${ENVIRONMENT}/pttg-fs-api-svc.yaml
