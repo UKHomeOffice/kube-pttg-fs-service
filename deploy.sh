27f66027c08f174ec3cb03cc48ff25c5c0962832
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-07-19 16:12:10 +0100

    try with pttg-fs-stub config

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-export KUBE_NAMESPACE=${KUBE_NAMESPACE}
-export ENVIRONMENT=${ENVIRONMENT}
-export APP=pttg-fs-service
+export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
+export ENVIRONMENT=${ENVIRONMENT:-dev}
+export APP=${APP:-pttg-fs-service}
 export KUBE_SERVER=${KUBE_SERVER_DEV}
-export KUBE_TOKEN=${KUBE_TOKEN}
-
-echo "Diagnosing .. got into the deploy.sh script"
 
 cd kd
-kd --debug \
-   --insecure-skip-tls-verify  --timeout 5m0s \
-   --file ${ENVIRONMENT}/pttg-fs-service-deployment.yaml \
-   --file ${ENVIRONMENT}/pttg-fs-service-svc.yaml
+kd --insecure-skip-tls-verify \
+   --file ${APP}-deployment.yaml \
+   --file ${APP}-svc.yaml \
+   --retries 50
