41d8abf16fa26697aaf4c0d2bd39abeba65f6a49
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-07-20 14:04:22 +0100

    add debug

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@ export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
 export ENVIRONMENT=${ENVIRONMENT:-dev}
 export APP=${APP:-pttg-fs-service}
 export KUBE_SERVER=${KUBE_SERVER_DEV}
+export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-echo "in deploy.sh"
+echo "in deploy.sh, with KUBE_TOKEN=" $KUBE_TOKEN
+env
 
 #kd --insecure-skip-tls-verify \
 #   --file ${APP}-deployment.yaml \
