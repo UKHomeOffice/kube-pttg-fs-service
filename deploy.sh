2e22f64c2e07111b7171da9ece7f8ca37b557450
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-03-07 15:26:54 +0000

    ACP_MIGRATION: add logging

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,14 @@ export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
 
 if [ ${ENVIRONMENT} == "prod" ] ; then
+    echo "deploy to prod namespace, using PTTG_FS_PROD drone secret"
     export KUBE_TOKEN=${PTTG_FS_PROD}
 else
     if [ ${ENVIRONMENT} == "test" ] ; then
+        echo "deploy to test namespace, using PTTG_FS_TEST drone secret"
         export KUBE_TOKEN=${PTTG_FS_TEST}
     else
+        echo "deploy to dev namespace, using PTTG_FS_DEV drone secret"
         export KUBE_TOKEN=${PTTG_FS_DEV}
     fi
 fi
