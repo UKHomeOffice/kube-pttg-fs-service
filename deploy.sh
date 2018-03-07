c16c6fc9a98e3fffbec95955e81241a9cf0630e2
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-03-07 15:07:48 +0000

    ACP_MIGRATION: specify namespace tokens

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,19 @@
 
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
-export KUBE_TOKEN=${KUBE_TOKEN}
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+if [ ${ENVIRONMENT} == "prod" ] ; then
+    export KUBE_TOKEN=${PTTG_FS_PROD}
+else
+    if [ ${ENVIRONMENT} == "test" ] ; then
+        export KUBE_TOKEN=${PTTG_FS_TEST}
+    else
+        export KUBE_TOKEN=${PTTG_FS_DEV}
+    fi
 fi
 
 cd kd
+
 kd --insecure-skip-tls-verify \
     -f networkPolicy.yaml \
     -f deployment.yaml \
